49b3159b2f9ddb5059500bea66b9ca8a4a2eb378
Larsen <Martin.Kvisvik.Larsen@dnvgl.com>
2019-07-04 16:09:33 +0200

    Fixed HomeController.

--- BridgeMVC/BridgeMVC/Controllers/HomeController.cs
+++ BridgeMVC/BridgeMVC/Controllers/HomeController.cs
@@ -1,19 +1,17 @@
-﻿using BridgeMVC.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using System.Diagnostics;
 using System.Net;
+using System.Threading.Tasks;
+using BridgeMVC.Models;
 using System.Security.Claims;
 using BridgeMVC.Extensions;
 
 namespace BridgeMVC.Controllers
 {
     public class HomeController : Controller
-
     {
         [Authorize]
         [ActionName("Index")]
@@ -24,22 +22,21 @@ namespace BridgeMVC.Controllers
             string userName = user.Email().ToLower();
             if (userName.Contains("dnvgl.com"))
             {
-                var users = await DocumentDBRepository.GetItemsAsync<BUser>(d => d.Tag == "BUser" && d.Email.ToLower() == userName);
+                var users = await DocumentDBRepository.GetItemsAsync<BUser>(
+                    d => d.Tag == "BUser" && d.Email.ToLower() == userName);
 
-                if (users != null)
-                {
+                if (users != null) {
                     BUser u = users.FirstOrDefault();
                     id = u.Id;
                     Session["BridgeModule"] = u.BridgeLastUsed;
                     Session["UserSignature"] = u.Signature;
-                    var lbb = await DocumentDBRepository.GetItemsAsync<BBridge>(d => d.Tag == "BBridge");
+                    var lbb = await DocumentDBRepository.GetItemsAsync<BBridge>(
+                        d => d.Tag == "BBridge");
                     ViewBag.bridges = lbb;
                 }
-
             }
             else
             {
-                
                 return RedirectToAction("SignUp", "Onboarding");
             }
 
@@ -49,13 +46,14 @@ namespace BridgeMVC.Controllers
             }
 
             BUser item = await DocumentDBRepository.GetItemAsync<BUser>(id);
+
             if (item == null)
             {
                 return HttpNotFound();
             }
+
             return View(item);
         }
-
 
         [Authorize]
         [HttpPost]
@@ -65,33 +63,23 @@ namespace BridgeMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-                await DocumentDBRepository.UpdateItemAsync<BUser>(item.Id, item);
-                
+                await DocumentDBRepository.UpdateItemAsync<BUser>(item.Id, item);       
             }
-             Session["BridgeModule"] = item.BridgeLastUsed;
+            Session["BridgeModule"] = item.BridgeLastUsed;
             return RedirectToAction("_Index", "Job");
         }
 
-        public ActionResult About()
-        {
-            return View();
-        }
-
-        // GET: BList
+        // Css demonstration method. Has nothing to do with Bridge.
         [ActionName("CSSDemo")]
         public ActionResult CSSDemo()
         {
-
             return View();
         }
 
         [ActionName("Settings")]
         public ActionResult Settings()
         {
-
             return View();
         }
-
-
     }
 }
